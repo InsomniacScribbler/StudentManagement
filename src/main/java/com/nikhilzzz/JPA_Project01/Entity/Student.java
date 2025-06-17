d275aeee8a567699491394012603cf61fc5d41f5
InsomniacScribbler <singhnikhilsingh21@gmail.com>
2025-06-17 11:50:06 +0530

    Created student Entity

---
+++ src/main/java/com/nikhilzzz/JPA_Project01/Entity/Student.java
@@ -0,0 +1,59 @@
+package com.nikhilzzz.JPA_Project01.Entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "students", uniqueConstraints = {
+        @UniqueConstraint(columnNames = {"email"})
+})
+public class Student {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String name;
+    private String email;
+    private int age;
+
+    public Student(String name, String email, int age) {
+        this.name = name;
+        this.email = email;
+        this.age = age;
+    }
+
+    public Student() {}  // Required by JPA
+
+    // Getters and Setters
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
